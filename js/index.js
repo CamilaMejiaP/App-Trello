539604a1144db48de37478ca7578745ca9f033ff
camila <camimcp21@gmail.com>
2022-07-31 22:16:35 -0500

    App Trello Finalizada

--- js/index.js
+++ js/index.js
@@ -41,30 +41,7 @@ Sortable.create(list3,{
     animation: 150
  }
 );
-/* 
-local storage: Metodo para almacenar datos en el local storage.
-PD: no implementado
-var guardado;
 
-function showTask() {
-    
-    localStorage.getItem('datos')
-    //remover del localstore
-    localStorage.removeItem('datos')
-   
-}
-guardado = JSON.parse(localStorage.getItem('datos'));
-if (guardado != null) {
-    for (i in guardado) {
-        addTask(guardado[i]);
-    }
-}
-
-else {
-    guardado = [];
-} */
-
-//funcion flecha que agrega una tarea nueva, cuando se llama al boton de submit
 
 btn.addEventListener('click', (e) => {
     e.preventDefault()
@@ -73,23 +50,10 @@ btn.addEventListener('click', (e) => {
 
 function addTask() {
 
-    /* const tarea = list.innerHTML;
-
-    list.innerHTML += `
-    <div class="tarea m-2 p-2" id="${contador}">
-        <h4>${tltTask.value}</h4>
-        <hr>
-        <p class="float">${txtDesTask.value}</p>
-        <br>
-        <h6>Responsable:${txtResponsable.value}<br> Fecha: ${date.value}</h6>
-    </div>  
-    `
-    */
-    //let contador = 0;
-    //crear elemento tarea
+  
     var newTask = document.createElement('div');
     newTask.setAttribute('class', 'tarea m-2 p-2');
-    //newTask.setAttribute('id', `id${contador}`);
+ 
 
     newTask.innerHTML += `
         <h4>${tltTask.value}</h4>
@@ -105,4 +69,3 @@ function addTask() {
 
 }
 
- //window.addEventListener("load", showTask, false); 
